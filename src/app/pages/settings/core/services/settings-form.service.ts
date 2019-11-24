14a2f8f2e60810c57058b06eca2e54380f7c01ba
Vladimir Zubkov <vladimirzubkov@v-zubkov.local>
2019-11-24 17:07:37 +0200

    Update translates

--- src/app/pages/settings/core/services/settings-form.service.ts
+++ src/app/pages/settings/core/services/settings-form.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { FormArray, FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 import { UTypes } from '@app/pages/users/core/types/users.types';
 
 @Injectable()
 export class SettingsFormService {
     public form = this.fb.group({
         // Main Info
-        fname: [''],
+        fname: ['', [Validators.required, Validators.maxLength(32)]],
         gender: [UTypes.EGender.MALE],
         phone: [null],
         city: [UTypes.ECity.KHARKOV],
@@ -16,7 +16,7 @@ export class SettingsFormService {
         avatar: ['assets/icon/logo.png'],
 
         // Additional Info
-        lname: [''],
+        lname: ['', [Validators.required,  Validators.maxLength(32)]],
         pname: [''],
         email: [''],
         height: [null],
