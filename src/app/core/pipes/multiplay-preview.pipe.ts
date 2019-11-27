f96d6bfdf730c3f76cd76a76612107983da388b7
SergPaltsev <you@example.com>
2019-11-27 22:40:55 +0200

    merge

--- src/app/core/pipes/multiplay-preview.pipe.ts
+++ src/app/core/pipes/multiplay-preview.pipe.ts
@@ -1,17 +1,18 @@
-import {Pipe, PipeTransform} from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
-	name: 'multiplayPreview'
+    name: 'multiplayPreview'
 })
-export class MultiplayPreviewPipe implements
-PipeTransform { 
-	constructor(private santinizer: DomSanitizer) {}
-	transform(items: any[]): SafeHtml {
-		return	items.length === 1
-		? items[0].name
-		: this.santinizer.bypassSecurityTrustHtml(
-			`<ion-badge color="primary">${items.length}</ion-badge>`,
-			);	 
-	}
+export class MultiplayPreviewPipe implements PipeTransform {
+    constructor(private santinizer: DomSanitizer) {
+    }
+
+    transform(items: any[]): SafeHtml {
+        return items.length === 1
+            ? items[0].name
+            : this.santinizer.bypassSecurityTrustHtml(
+                `<ion-badge color="primary">${items.length}</ion-badge>`,
+            );
+    }
 }
