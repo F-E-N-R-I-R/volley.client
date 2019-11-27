f02be184dd9b62fb5f36ec96f95be4e07be9e69a
Papavel22 <pavel.starusev@gmail.com>
2019-11-27 22:39:02 +0200

    add bodge

--- src/app/core/pipes/multiplay-preview.pipe.ts
+++ src/app/core/pipes/multiplay-preview.pipe.ts
@@ -1,13 +1,17 @@
 import {Pipe, PipeTransform} from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
 	name: 'multiplayPreview'
 })
 export class MultiplayPreviewPipe implements
 PipeTransform { 
-	transform(items: any[]) {
-		return	items.length === 1 ? items[0].name : items.length;
-		 
+	constructor(private santinizer: DomSanitizer) {}
+	transform(items: any[]): SafeHtml {
+		return	items.length === 1
+		? items[0].name
+		: this.santinizer.bypassSecurityTrustHtml(
+			`<ion-badge color="primary">${items.length}</ion-badge>`,
+			);	 
 	}
-
 }
