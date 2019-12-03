7d073618921e6e16309228d737680b3f497b029c
PaltsevSerg <PaltsevSerg@users.noreply.github.com>
2019-12-03 17:57:25 +0200

    add pipe "phone"

--- src/app/core/pipes/phone.pipe.ts
+++ src/app/core/pipes/phone.pipe.ts
@@ -1,18 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
     name: 'phone'
 })
 export class PhonePipe implements PipeTransform {
-    constructor(private sanitizer: DomSanitizer) {
-    }
 
-    transform(phone: number): SafeHtml {
-        return this.sanitizer.bypassSecurityTrustHtml(
-            (String(phone).length === 9) ?
-                `<span>${String(phone).replace(/(\d{2})(\d{3})(\d{4})/, '+380 $1 $2 $3')}</span>` :
-                `<span>${String(phone)}</span>`,
-        );
+    transform(phone: number, collable?: boolean): SafeHtml {
+            return `<a href="tel:+380501234567">
+                        ${String(phone).replace(/(\d{2})(\d{3})(\d{4})/, '+380 $1 $2 $3')}
+                    </a>`;
     }
 }
