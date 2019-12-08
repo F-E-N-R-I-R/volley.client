a9431885a98ba830949afc17385f2826777e33fd
Papavel22 <pavel.starusev@gmail.com>
2019-12-08 21:06:25 +0200

    fixed toggle

--- src/app/pages/settings/notifications/notifications.page.ts
+++ src/app/pages/settings/notifications/notifications.page.ts
@@ -1,34 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { SettingsFormService } from '@app/pages/settings/core/services/settings-form.service';
 import { AuthService } from '@app/pages/auth/core/services/auth.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
     templateUrl: 'notifications.page.html',
     styleUrls: ['notifications.page.scss'],
 })
-export class NotificationsPage implements OnInit {
+export class NotificationsPage implements OnInit, OnDestroy {
+    private ngUnsubscribe$ = new Subject();
     public form = this.settingsFormService.form.get('notifications') as FormGroup;
-    public all: boolean;
+    public allToggleControl: FormControl = new FormControl(false);
 
     constructor(private authService: AuthService, private settingsFormService: SettingsFormService) {
     }
 
     ngOnInit(): void {
-        this.form.valueChanges.subscribe((value) => {
-            this.all = Object.values(value).every(Boolean);
-        });
+        this.form.valueChanges
+            .pipe(
+                takeUntil(this.ngUnsubscribe$),
+            )
+            .subscribe((value) => {
+                this.allToggleControl.setValue(
+                    Object.values(value).every(Boolean),
+                    { emitEvent: false }
+                );
+            });
+
+
+        this.allToggleControl.valueChanges
+            .pipe(
+                takeUntil(this.ngUnsubscribe$),
+            )
+            .subscribe((value) => {
+                const notificationGroup = this.form as FormGroup;
+
+                Object.keys(notificationGroup.controls).forEach(controlName => {
+                    notificationGroup.controls[controlName].setValue(value);
+                });
+            });
     }
+
+    ngOnDestroy() {
+        this.ngUnsubscribe$.next();
+        this.ngUnsubscribe$.complete();
+    } 
 
     public ionViewWillLeave() {
         this.authService.dispatchSilentUpdate(this.settingsFormService.form.value);
-    }
-
-    public onAllToggle($e) {
-        const notificationGroup = this.form as FormGroup;
-
-        Object.keys(notificationGroup.controls).forEach(controlName => {
-            notificationGroup.controls[controlName].setValue($e.detail.checked);
-        });
     }
 }
