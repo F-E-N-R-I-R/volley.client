c97a47f602080b4c7b0cd49dcfd4854bb5946947
SergPaltsev <you@example.com>
2019-12-22 22:01:52 +0200

    apply localization to date picker

--- src/app/pages/settings/main-info/main-info.page.ts
+++ src/app/pages/settings/main-info/main-info.page.ts
@@ -1,20 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { SettingsFormService } from '@app/pages/settings/core/services/settings-form.service';
 import { AuthService } from '@app/pages/auth/core/services/auth.service';
 import { EnumValues } from 'enum-values';
 import { UTypes } from '@app/pages/users/core/types/users.types';
 import { NavController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
+import { map, takeUntil } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
     templateUrl: 'main-info.page.html',
     styleUrls: ['main-info.page.scss'],
 })
-export class MainInfoPage {
+export class MainInfoPage implements OnDestroy {
+    private ngUnsubscribe$ = new Subject();
+    public months$: Observable<string[]> = this.translateService.get('months')
+        .pipe(
+            takeUntil(this.ngUnsubscribe$),
+            map(months => Object.values(months))
+        );
     public form = this.settingsFormService.form as FormGroup;
     public genders = EnumValues.getValues(UTypes.EGender);
+    public customActionSheetOptions = {
+        color: 'vua-secondary',
+        translucent: true
+    };
 
-    constructor(private authService: AuthService, private settingsFormService: SettingsFormService, private navController: NavController) {
+    constructor(private authService: AuthService,
+                private settingsFormService: SettingsFormService,
+                private navController: NavController,
+                private translateService: TranslateService) {
+    }
+
+    ngOnDestroy(): void {
+        this.ngUnsubscribe$.next();
+        this.ngUnsubscribe$.complete();
     }
 
     public save() {
