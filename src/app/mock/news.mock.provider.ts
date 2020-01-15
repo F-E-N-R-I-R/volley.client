348f0ff62cf9bf2120fbe6339ac0a4f2cb23954f
SergPaltsev <you@example.com>
2020-01-15 22:25:20 +0200

    add types and mock news

--- src/app/mock/news.mock.provider.ts
+++ src/app/mock/news.mock.provider.ts
@@ -9,9 +9,30 @@ export class NewsMockProvider {
     getAllNews(): Observable<NTypes.INewsList> {
         return new BehaviorSubject({
             items: [
+<<<<<<< HEAD
                 { name: 'News1', id: 1 },
                 { name: 'News2', id: 2 },
                 { name: 'News3', id: 3 },
+=======
+                {   title: 'New1',
+                    picture: 'string',
+                    description: 'string',
+                    category: NTypes.ECategory.FVU,
+                    date: 'string',
+                    created_by: 'string', },
+                {   title: 'New2',
+                    picture: 'string',
+                    description: 'string',
+                    category: NTypes.ECategory.FVHO,
+                    date: 'string',
+                    created_by: 'string', },
+                {   title: 'New3',
+                    picture: 'string',
+                    description: 'string',
+                    category: NTypes.ECategory.FIVB,
+                    date: 'string',
+                    created_by: 'string', },
+>>>>>>> add types and mock news
             ]
         });
     }
