d6cd8ec70e65fd1a8aa1fa434721e25f6354d4a3
Papavel22 <pavel.starusev@gmail.com>
2020-01-26 20:06:16 +0200

    fix news lent

--- src/app/pages/news/components/list-item/news-list-item.component.ts
+++ src/app/pages/news/components/list-item/news-list-item.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NTypes } from '@core/types';
 
 @Component({
-    selector: 'news-list-item',
+    selector: 'app-news-list-item',
     templateUrl: 'news-list-item.component.html',
     styleUrls: ['news-list-item.component.scss'],
 })
-export class NewsListItemComponent {}
+export class NewsListItemComponent {
+	@Input() item: NTypes.INews;
+	@Input() description: NTypes.INews;
+}
