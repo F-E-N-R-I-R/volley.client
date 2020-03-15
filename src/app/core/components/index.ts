ba71cc1fef1959358dc4aaf2ab699ee7c49a53e8
Vladimir Zubkov <vladimirzubkov@v-zubkov.local>
2020-03-15 22:00:00 +0200

    Added actions component

--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -1,2 +1,3 @@
 export * from '@core/components/error-list/error-list.component';
 export * from '@core/components/tabs/tabs.page';
+export * from '@core/components/item-actions/item-actions.component';
